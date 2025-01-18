7260a2b2c78fc522606f094267fc2433e4fcd1e9
Chris Cummer <ccummer@digitalocean.com>
2025-01-18 07:46:09 -0800

    Add option to colorize output:
    Signed-off-by: Chris Cummer <ccummer@digitalocean.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/senorprogrammer/uuid
 
 go 1.23.1
 
-require github.com/gofrs/uuid v4.4.0+incompatible
+require (
+	github.com/fatih/color v1.18.0
+	github.com/gofrs/uuid v4.4.0+incompatible
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,11 +5,20 @@ import (
 	"fmt"
 	"log"
 
+	"math/rand/v2"
+
+	"github.com/fatih/color"
 	"github.com/gofrs/uuid"
+)
+
+const (
+	min = 32
+	max = 256
 )
 
 func main() {
 	uuidCount := flag.Int("count", 1, "number of uuids to generate")
+	withColor := flag.Bool("color", false, "display in random color")
 
 	flag.Parse()
 
@@ -18,6 +27,16 @@ func main() {
 		if err != nil {
 			log.Fatalf("Failed to generate UUID: %v", err)
 		}
-		fmt.Println(uuid)
+
+		if !*withColor {
+			fmt.Println(uuid.String())
+			continue
+		}
+
+		r := rand.IntN(max-min+1) + min
+		g := rand.IntN(max-min+1) + min
+		b := rand.IntN(max-min+1) + min
+
+		color.RGB(r, g, b).Println(uuid)
 	}
 }
